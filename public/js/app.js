6b62066aaa2d829ad6536189e9b12139d6afd738
Gum San Aung <aunggum@hawaii.edu>
2021-03-16 18:39:02 +0000

    Added languages data

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
+window.onload = getLanguages;
+
 function setLanguage(name) {
     localStorage.setItem('lang', name);
+}
+
+async function getLanguages() {
+    const url = "assets/data/languages.json";
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
 }
